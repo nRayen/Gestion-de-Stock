01824d66b7c18033d02249e7a3ba4b3a82a0478e
nRayen <rayen.ndiaye@gmail.com>
2024-05-03 12:25:48 +0200

    Ajout classes Vente et VenteDAO

--- src/Stock/Vente.java
+++ src/Stock/Vente.java
@@ -4,6 +4,35 @@ public class Vente {
 
     public int id;
     public String clientName;
-    public int price;
-    public Produit[] produitsVendus;
+    public float total;
+
+    public Vente(String client) {
+        this.clientName = client;
+    }
+
+    // Getters
+    public int getId() {
+        return this.id;
+    }
+
+    public String getClientName() {
+        return this.clientName;
+    }
+
+    public float getTotal() {
+        return this.total;
+    }
+
+    // Setters
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    public void setClientName(String value) {
+        this.clientName = value;
+    }
+
+    public void setTotal(float value) {
+        this.total = value;
+    }
 }
